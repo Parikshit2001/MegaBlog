acd23542e97be2552294cdd40c8a1d1c8532bc03
Parikshit Gupta <parikshitgupta@Parikshits-MacBook-Air.local>
2024-01-15 22:16:09 +0530

    nit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -15,21 +15,20 @@ function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const login = async(e) => {
-        e.preventDefault();
+    const login = async(data) => {
         setError("")
         try {
             authService.login({email, password})
-            .then((session) => {
-                if (session) {
-                    return authService.getCurrentUser()
-                }
-            })
+            .then((session) => authService.getCurrentUser())
             .then((userData) => {
                 if(userData) {
                     dispatch(authLogin(userData));
                     navigate("/all-posts")
                 }
+            })
+            .finally(() => {
+                setEmail('')
+                setPassword('')
             })
         } catch (error) {
             setError(error.message)
@@ -57,7 +56,7 @@ function Login() {
                     </Link>
         </p>
         {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
-        <form onSubmit={() => login(e)} className='mt-8'>
+        <form onSubmit={handleSubmit(login)} className='mt-8'>
             <div className='space-y-5'>
                 <Input
                 label="Email: "
